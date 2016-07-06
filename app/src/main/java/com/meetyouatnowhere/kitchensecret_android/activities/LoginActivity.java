e0848fe019ba1448ca5b98ae1c3c1974956853e7
CharleneYuan <953812979@qq.com>
2016-07-06 10:07:24 +0800

    7.6 规范工程

---
+++ app/src/main/java/com/meetyouatnowhere/kitchensecret_android/activities/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.meetyouatnowhere.kitchensecret_android.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login1);
+    }
+}
--- app/src/main/java/com/meetyouatnowhere/kitchensecret_android/activities/MainActivity.java
+++ app/src/main/java/com/meetyouatnowhere/kitchensecret_android/activities/MainActivity.java
@@ -1,15 +1,38 @@
 package com.meetyouatnowhere.kitchensecret_android.activities;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.meetyouatnowhere.kitchensecret_android.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn_recipe;
+    private Button btn_myCommunity;
+    private Button btn_search;
+    private Button btn_mySetting;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_recipe = (Button) findViewById(R.id.recipe_btn);
+        btn_myCommunity = (Button) findViewById(R.id.myCommunity_btn);
+        btn_search = (Button) findViewById(R.id.search_btn);
+        btn_mySetting = (Button) findViewById(R.id.mySetting_btn);
+
+       // FragmentManager fragmentManager = getSupportFragmentManager();
+       // fragmentManager.beginTransaction().replace(R.id.contentFrame,new )
+
+        btn_recipe.setOnClickListener(new View.OnClickListener(){
+
+        });
     }
+
+
 }
