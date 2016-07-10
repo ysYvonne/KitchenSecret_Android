7a7b7e2a3b13e8e574d4c9c6c11970b6d68daed6
CharleneYuan <953812979@qq.com>
2016-07-10 19:11:10 +0800

    7.10 实现首页瀑布流

--- app/src/main/java/com/meetyouatnowhere/kitchensecret_android/activities/Add_recipeActivity.java
+++ app/src/main/java/com/meetyouatnowhere/kitchensecret_android/activities/Add_recipeActivity.java
@@ -3,6 +3,8 @@ package com.meetyouatnowhere.kitchensecret_android.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.meetyouatnowhere.kitchensecret_android.R;
+
 public class Add_recipeActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/meetyouatnowhere/kitchensecret_android/activities/RecipeFragment.java
+++ app/src/main/java/com/meetyouatnowhere/kitchensecret_android/activities/RecipeFragment.java
@@ -2,6 +2,7 @@ package com.meetyouatnowhere.kitchensecret_android.activities;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -10,14 +11,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
+
+import com.dodowaterfall.Helper;
+import com.dodowaterfall.widget.ScaleImageView;
 import com.meetyouatnowhere.kitchensecret_android.activities.bannerview.ImagePagerAdapter;
 import com.meetyouatnowhere.kitchensecret_android.R;
 import com.meetyouatnowhere.kitchensecret_android.activities.bannerview.CircleFlowIndicator;
 import com.meetyouatnowhere.kitchensecret_android.activities.bannerview.ViewFlow;
+import com.meetyouatnowhere.kitchensecret_android.activities.pulldown.PullToRefreshSampleActivity;
+import com.meetyouatnowhere.kitchensecret_android.activities.view.XListView;
+import com.meetyouatnowhere.kitchensecret_android.util.ImageFetcher;
+import com.model.DuitangInfo;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,7 +43,7 @@ import java.util.ArrayList;
  * Use the {@link RecipeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RecipeFragment extends Fragment{
+public class RecipeFragment extends Fragment implements XListView.IXListViewListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -39,11 +56,174 @@ public class RecipeFragment extends Fragment{
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
+    private ImageFetcher mImageFetcher;
+    private XListView mAdapterView = null;
+    private StaggeredAdapter mAdapter = null;
+    private int currentPage = 0;
+    ContentTask task = new ContentTask(this.getActivity(), 2);
 //    private OnFragmentInteractionListener mListener;
+private class ContentTask extends AsyncTask<String, Integer, List<DuitangInfo>> {
 
+    private Context mContext;
+    private int mType = 1;
+
+    public ContentTask(Context context, int type) {
+        super();
+        mContext = context;
+        mType = type;
+    }
+
+    @Override
+    protected List<DuitangInfo> doInBackground(String... params) {
+        try {
+            return parseNewsJSON(params[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(List<DuitangInfo> result) {
+        if (mType == 1) {
+
+            mAdapter.addItemTop(result);
+            mAdapter.notifyDataSetChanged();
+            mAdapterView.stopRefresh();
+
+        } else if (mType == 2) {
+            mAdapterView.stopLoadMore();
+            mAdapter.addItemLast(result);
+            mAdapter.notifyDataSetChanged();
+        }
+
+    }
+
+    @Override
+    protected void onPreExecute() {
+    }
+
+    public List<DuitangInfo> parseNewsJSON(String url) throws IOException {
+        List<DuitangInfo> duitangs = new ArrayList<DuitangInfo>();
+        String json = "";
+        if (Helper.checkConnection(mContext)) {
+            try {
+                json = Helper.getStringFromUrl(url);
+
+            } catch (IOException e) {
+                Log.e("IOException is : ", e.toString());
+                e.printStackTrace();
+                return duitangs;
+            }
+        }
+        Log.d("RecipeFragment", "json:" + json);
+
+        try {
+            if (null != json) {
+                JSONObject newsObject = new JSONObject(json);
+                JSONObject jsonObject = newsObject.getJSONObject("data");
+                JSONArray blogsJson = jsonObject.getJSONArray("blogs");
+
+                for (int i = 0; i < blogsJson.length(); i++) {
+                    JSONObject newsInfoLeftObject = blogsJson.getJSONObject(i);
+                    DuitangInfo newsInfo1 = new DuitangInfo();
+                    newsInfo1.setAlbid(newsInfoLeftObject.isNull("albid") ? "" : newsInfoLeftObject.getString("albid"));
+                    newsInfo1.setIsrc(newsInfoLeftObject.isNull("isrc") ? "" : newsInfoLeftObject.getString("isrc"));
+                    newsInfo1.setMsg(newsInfoLeftObject.isNull("msg") ? "" : newsInfoLeftObject.getString("msg"));
+                    newsInfo1.setHeight(newsInfoLeftObject.getInt("iht"));
+                    duitangs.add(newsInfo1);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return duitangs;
+    }
+}
     public RecipeFragment() {
         // Required empty public constructor
+    }
+    /**
+     * 添加内容
+     *
+     * @param pageindex
+     * @param type
+     *            1为下拉刷新 2为加载更多
+     */
+    private void AddItemToContainer(int pageindex, int type) {
+        if (task.getStatus() != AsyncTask.Status.RUNNING) {
+            String url = "http://www.duitang.com/album/1733789/masn/p/" + pageindex + "/24/";
+            Log.d("MainActivity", "current url:" + url);
+            ContentTask task = new ContentTask(this.getActivity(), type);
+            task.execute(url);
+
+        }
+    }
+
+    public class StaggeredAdapter extends BaseAdapter {
+        private Context mContext;
+        private LinkedList<DuitangInfo> mInfos;
+        private XListView mListView;
+
+        public StaggeredAdapter(Context context, XListView xListView) {
+            mContext = context;
+            mInfos = new LinkedList<DuitangInfo>();
+            mListView = xListView;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder;
+            DuitangInfo duitangInfo = mInfos.get(position);
+
+            if (convertView == null) {
+                LayoutInflater layoutInflator = LayoutInflater.from(parent.getContext());
+                convertView = layoutInflator.inflate(R.layout.infos_list, null);
+                holder = new ViewHolder();
+                holder.imageView = (ScaleImageView) convertView.findViewById(R.id.news_pic);
+                holder.contentView = (TextView) convertView.findViewById(R.id.news_title);
+                convertView.setTag(holder);
+            }
+
+            holder = (ViewHolder) convertView.getTag();
+            holder.imageView.setImageWidth(duitangInfo.getWidth());
+            holder.imageView.setImageHeight(duitangInfo.getHeight());
+            holder.contentView.setText(duitangInfo.getMsg());
+            mImageFetcher.loadImage(duitangInfo.getIsrc(), holder.imageView);
+            return convertView;
+        }
+
+        class ViewHolder {
+            ScaleImageView imageView;
+            TextView contentView;
+            TextView timeView;
+        }
+
+        @Override
+        public int getCount() {
+            return mInfos.size();
+        }
+
+        @Override
+        public Object getItem(int arg0) {
+            return mInfos.get(arg0);
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            return 0;
+        }
+
+        public void addItemLast(List<DuitangInfo> datas) {
+            mInfos.addAll(datas);
+        }
+
+        public void addItemTop(List<DuitangInfo> datas) {
+            for (DuitangInfo info : datas) {
+                mInfos.addFirst(info);
+            }
+        }
     }
 
     /**
@@ -88,6 +268,15 @@ public class RecipeFragment extends Fragment{
         imageUrlList
                 .add("http://photocdn.sohu.com/20150422/mp11846883_1429673028490_15.jpeg");
         initBanner(imageUrlList);
+
+        mAdapterView = (XListView)view.findViewById(R.id.list);
+        mAdapterView.setPullLoadEnable(true);
+        mAdapterView.setXListViewListener(this);
+
+        mAdapter = new StaggeredAdapter(this.getActivity(), mAdapterView);
+
+        mImageFetcher = new ImageFetcher(this.getActivity(), 240);
+        mImageFetcher.setLoadingImage(R.mipmap.empty_photo);
         return view;
     }
 
@@ -147,4 +336,22 @@ public class RecipeFragment extends Fragment{
 //        // TODO: Update argument type and name
 //        void onFragmentInteraction(Uri uri);
 //    }
+        @Override
+        public void onResume() {
+            super.onResume();
+            mImageFetcher.setExitTasksEarly(false);
+            mAdapterView.setAdapter(mAdapter);
+            AddItemToContainer(currentPage, 2);
+        }
+    @Override
+    public void onRefresh() {
+        AddItemToContainer(++currentPage, 1);
+
+    }
+
+    @Override
+    public void onLoadMore() {
+        AddItemToContainer(++currentPage, 2);
+
+    }
 }
