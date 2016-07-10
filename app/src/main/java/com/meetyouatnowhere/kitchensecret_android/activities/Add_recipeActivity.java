3bfe99c25a23cc082d88a77706191e37dda4635f
ysYvonne <charlottejo@homtail.com>
2016-07-10 12:00:25 +0800

    7.10 全部界面完成

---
+++ app/src/main/java/com/meetyouatnowhere/kitchensecret_android/activities/Add_recipeActivity.java
@@ -0,0 +1,13 @@
+package com.meetyouatnowhere.kitchensecret_android.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Add_recipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_recipe);
+    }
+}
